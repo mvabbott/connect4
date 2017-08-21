83f6f6bf75214d78122cebe76680a6213490d0aa
Matt Abbott <matt.v.abbott@gmail.com>
2017-08-21 11:53:09 -0400

    Alternate between 2 players.

--- app/models/game_board.rb
+++ app/models/game_board.rb
@@ -1,21 +1,27 @@
 class GameBoard
-  attr_reader :num_col, :num_row
+  attr_reader :num_col, :num_row, :next_player
 
   def initialize
     @num_col = 7
     @num_row = 6
     @cells = Array.new(@num_col) { Array.new(@num_row) { Cell.new } }
+    @next_player = 1
   end
 
   def drop(col)
     # Using fetch to cause IndexError rather than return nil
     # TODO Look for a cleaner way to raise errors (with friendly messages)
     row = @cells.fetch(col).rindex { |cell| cell.available? }
-    @cells[col].fetch(row).assign(1)
+    @cells[col].fetch(row).assign(@next_player)
+    update_next_player
   end
 
   def playerAt(col, row)
     @cells[col][row].owner
+  end
+
+  def update_next_player
+    @next_player = @next_player == 1 ? 2 : 1
   end
 
   def display
--- test/models/game_board_test.rb
+++ test/models/game_board_test.rb
@@ -6,8 +6,7 @@ class GameBoardTest < ActiveSupport::TestCase
   end
 
   test "drop fills bottom row" do
-    @game.drop(1)
-    assert_equal 1, @game.playerAt(1, 5), "expected cell has player 1"
+    dropAndValidate(1, @game.num_row - 1, 1)
     assert_equal 0, @game.playerAt(1, 4), "next row up is filled"
   end
 
@@ -17,11 +16,21 @@ class GameBoardTest < ActiveSupport::TestCase
 
   test "Error when dropping to full column" do
     # fill up column
-    @game.num_row.times { @game.drop(1) }
-    (0..@game.num_row-1).each { |row| assert_equal 1, @game.playerAt(1, row) }
+    @game.num_row.times { |i| dropAndValidate(1, @game.num_row - (i+1), i%2 + 1) }
 
     # try to add one more
     assert_raise { @game.drop(1) }
+  end
+
+  test "Drop alternates players" do
+    3.times do |i|
+      dropAndValidate(1, @game.num_row - (i+1), i%2 + 1)
+    end
+  end
+
+  def dropAndValidate(col, expectedRow, expectedPlayer)
+    @game.drop(col)
+    assert_equal expectedPlayer, @game.playerAt(col, expectedRow)
   end
 
 end
